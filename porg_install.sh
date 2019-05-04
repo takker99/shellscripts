406a6c2981cf09bea3f3303661aa34fd9ee7f39f
takker99 <electronic.homuhomu@gmail.com>
2019-05-05 08:21:10 +0900

    makeのcore数を修正

--- porg_install.sh
+++ porg_install.sh
@@ -3,6 +3,6 @@ wget https://sourceforge.net/projects/porg/files/porg-0.10.tar.gz
 tar xf porg-0.10.tar.gz
 cd porg-0.10
 nice -n 19 ./configure --prefix=$HOME/.local --sysconfdir=$HOME/.local/etc --with-porg-logdir=$HOME/.local/var/log/porg --disable-grop
-nice -n 19 make -j8
+nice -n 19 make -j4
 nice -n 19 mkdir -p $HOME/.local/var/log/porg
-./porg/porg -lD make install
+nice -n 19 ./porg/porg -lD make install
