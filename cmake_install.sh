b22fb75d17c49d3357fa5c3a4ab51d79124712cc
takker99 <electronic.homuhomu@gmail.com>
2019-05-05 16:01:56 +0900

    feature branch に移動

---
+++ cmake_install.sh
@@ -0,0 +1,11 @@
+cd $HOME/.local
+wget https://github.com/Kitware/CMake/releases/download/v3.14.3/cmake-3.14.3.tar.gz
+nice -n 19 tar -xf cmake-3.14.3.tar.gz
+cd cmake-3.14.3
+nice -n 19 ./configure --system-curl --prefix=$HOME/.local
+nice -n 19 make -j4
+nice -n 19 porg -lD "make install"
+cd
+cd $HOME/.local
+rm -r cmake-3.14.3/
+rm cmake-3.14.3.tar.gz
