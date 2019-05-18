65701bc163c295e16ea3bbd09b174bb7c4a12c2a
takker99 <electronic.homuhomu@gmail.com>
2019-05-18 17:45:48 +0900

    versionを変数で置いた

--- cmake_install.sh
+++ cmake_install.sh
@@ -1,11 +1,13 @@
+#!/bin/sh
+cmake_version="3.14.3"
 cd $HOME/.local
-wget https://github.com/Kitware/CMake/releases/download/v3.14.3/cmake-3.14.3.tar.gz
-nice -n 19 tar -xf cmake-3.14.3.tar.gz
-cd cmake-3.14.3
+wget https://github.com/Kitware/CMake/releases/download/v${cmake_version}/cmake-${cmake_version}.tar.gz
+nice -n 19 tar -xf cmake-${cmake_version}.tar.gz
+cd cmake-${cmake_version}
 nice -n 19 ./configure --prefix=$HOME/.local
-nice -n 19 make -j4
+nice -n 19 make -j${nproc}
 nice -n 19 porg -lD "make install"
 cd
 cd $HOME/.local
-rm -r cmake-3.14.3/
-rm cmake-3.14.3.tar.gz
+rm -r cmake-${cmake_version}/
+rm cmake-${cmake_version}.tar.gz
